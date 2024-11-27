9ac73a751500c2c842731e660fb1de508c83f4b1
Shershnyaga <leshchukr2004@gmail.com>
2024-11-27 20:30:02 +0300

    Lab4

---
+++ Lab4/Lab4/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Lab4;
+
+public static class Program
+{
+    public static void Main()
+    {
+        while (true)
+        {
+            Console.Write("Input string > ");
+            var input = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(input))
+            {
+                Console.WriteLine("Input string is empty.");
+                continue;
+            }
+
+            Console.Write("Input regex > ");
+            var filter = Console.ReadLine();
+            
+            while (true)
+            {
+                if (string.IsNullOrEmpty(filter))
+                {
+                    Console.WriteLine("Regex string is empty.");
+                    
+                    Console.Write("Input regex > ");
+                    filter = Console.ReadLine();
+                    continue;
+                }
+                
+                break;
+            }
+            
+            var words = input.Split(' ', '.', ',', ';', ':', '!', '?');
+            
+            Console.Write("Result: ");
+            var result = new StringBuilder();
+            foreach (var word in words)
+            {
+                if (Regex.IsMatch(word, filter, RegexOptions.IgnoreCase))
+                {
+                    result.Append(word + ", ");
+                }
+            }
+            
+            Console.WriteLine("Result: " + result);
+        }
+    }
+}
+
