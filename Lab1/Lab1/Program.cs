4b557a9f58e40009676d61b1e8ae85bb536165ec
Shershnyaga <leshchukr2004@gmail.com>
2024-11-27 18:32:28 +0300

    Lab1

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Lab1;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var rnd = new Random();
+
+        var array = new int[10];
+        for (var i = 0; i < 10; i++)
+        {
+            array[i] = rnd.Next(0, 100);
+        }
+        
+        Console.WriteLine("Non sorted: " + string.Join(", ", array));
+        Console.WriteLine("Sorted: " + string.Join(", ", Sorter.Sort(array)));
+    }
+}
---
+++ Lab1/Lab1/Sorter.cs
@@ -0,0 +1,34 @@
+﻿namespace Lab1
+{
+    public class Sorter
+    {
+        public static int[] Sort(int[] array)
+        {
+            var sortedArray =  array.Clone() as int[];
+        
+            while (true)
+            {
+                var isSorted = true;
+                for (var i = 0; i < sortedArray.Length - 1; i++)
+                {
+                    if (sortedArray[i] <= sortedArray[i + 1]) continue;
+                    
+                    isSorted = false;
+                    Swap(sortedArray, i);
+                }
+            
+                if (isSorted)
+                {
+                    break;
+                }
+            }
+
+            return sortedArray;
+        }
+
+        private static void Swap(int[] array, int index)
+        {
+            (array[index], array[index + 1]) = (array[index + 1], array[index]);
+        }
+    }
+}
